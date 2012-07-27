ae52e99f3ed79d2d51f7e733cf0282c97f9a8146
Josh Cunningham <josh@joshcanhelp.com>
2012-07-26 17:37:36 -0700

    first commit

---
+++ FormBuilder.php
@@ -0,0 +1,367 @@
+<?php
+class ThatFormBuilder {
+	
+	// Stores all form inputs
+	private $inputs = array(); 
+	
+	// Stores all form attributes
+	private $form = array(); 
+	
+	// Make sure a submit button is output
+	private $has_submit = false;
+	
+	/*
+	Constructor to set basic form attributes
+	*/
+	
+	function __construct($action = '', $args = false) {
+		
+		$defaults = array(
+			'action' => $action,
+			'method' => 'post',
+			'enctype' => 'application/x-www-form-urlencoded',
+			'class' => array(),
+			'id' => '',
+			'markup' => 'html',
+			'novalidate' => false,
+			'add_nonce' => false,
+			'add_honeypot' => true,
+		);
+		
+		if ($args) $settings = array_merge($defaults, $args);
+		else $settings = $defaults;
+		
+		foreach ($settings as $key => $val) :
+			// Try setting with user-passed setting
+			// If not, try the default with the same key name
+			if (!$this->set_att($key, $val)) $this->set_att($key, $defaults[$key]);
+		endforeach;
+	
+		
+	}
+	
+	function set_att($key, $val) {
+	
+		switch ($key) :
+			
+			case 'method':
+				if (! in_array($val, array('post', 'get'))) return false;
+				break;
+			
+			case 'enctype':
+				if (! in_array($val, array('application/x-www-form-urlencoded', 'multipart/form-data'))) return false;
+				break;
+			
+			case 'markup':
+				if (! in_array($val, array('html', 'xhtml'))) return false;
+				break;
+			
+			case 'class':
+			case 'id':
+				if (! $this->_check_valid_attr($val)) return false;
+				break;
+			
+			case 'novalidate':
+			case 'add_honeypot':
+				if (! is_bool($val)) return false;
+				break;
+			
+			case 'add_nonce':
+				if (! is_string($val) && !is_bool($val)) return false;
+				break;
+			
+			default: 
+				return false;
+			
+		endswitch;
+		
+		$this->form[$key] = $val;
+		
+		return true;
+		
+	}
+	
+	function add_input($label, $args = '', $slug = '') {
+		
+		if (empty($args)) $args = array();
+		// Create slug
+		if (empty($slug)) $slug = $this->_make_slug($label);
+		
+		$defaults = array(
+			'type' => 'text',
+			'name' => $slug,
+			'id' => $slug,
+			'label' => $label,
+			'value' => '',
+			'placeholder' => '',
+			'class' => array(),
+			'min' => '',
+			'max' => '',
+			'step' => '',
+			'autofocus' => false,
+			'checked' => false,
+			'required' => false,
+			'add_label' => true,
+			'options' => array(),
+			'wrap_tag' => 'div',
+			'wrap_class' => array('form_field_wrap'),
+			'wrap_id' => '',
+			'wrap_style' => ''
+		
+		);
+		
+		// Combined defaults and arguments
+		// Arguments override defaults
+		$args = array_merge($defaults, $args);
+		
+		$this->inputs[$slug] = $args;
+		
+	}
+	
+	function add_inputs($arr) {
+		
+		if (!is_array($arr)) return false;
+		
+		foreach ($arr as $field) :
+			$this->add_input($field[0], isset($field[1]) ? $field[1] : '', isset($field[2]) ? $field[2] : '');
+		endforeach;
+		
+	}
+	
+	function build_form() {
+	
+		$output = '
+		<form method="' . $this->form['method'] . '"';
+		
+		if (!empty($this->form['enctype'])) $output .= ' enctype="' . $this->form['enctype'] . '"';
+		
+		if (!empty($this->form['action'])) $output .= ' action="' . $this->form['action'] . '"';
+		
+		if (!empty($this->form['id'])) $output .= ' id="' . $this->form['id'] . '"';
+		
+		if (count($this->form['class']) > 0) $output .= $this->_output_classes($this->form['class']);
+		
+		if ($this->form['novalidate']) $output .= ' novalidate';
+		
+		$output .= '>';
+		
+		if ($this->form['add_honeypot']) 
+			$this->add_input('Leave blank to submit', array(
+				'name' => 'honeypot',
+				'slug' => 'honeypot',
+				'id' => 'form_honeypot',
+				'wrap_tag' => 'div',
+				'wrap_class' => array('form_field_wrap', 'hidden'),
+				'wrap_id' => '',
+				'wrap_style' => 'display: none'
+			));
+		
+		if ($this->form['add_nonce'] && function_exists('wp_create_nonce')) 
+			$this->add_input('WordPress nonce', array(
+				'name' => wp_create_nonce($this->form['add_nonce']),
+				'add_label' => false,
+				'type' => 'hidden'
+			));
+		
+		foreach ($this->inputs as $key => $val) :
+			
+			$min_max_range = $element = $end = $attr = $field = $label_html = '';
+			
+			// Set the field value to incoming
+			$val['value'] = isset($_REQUEST[$val['name']]) && !empty($_REQUEST[$val['name']]) ? 
+				$_REQUEST[$val['name']] : 
+				$val['value'];
+			
+			switch ($val['type']) :
+				
+				case 'title':
+					$element = '';
+					$end = '
+					<h3>' . $val['label'] . '</h3>';
+					break;
+				
+				case 'textarea':
+					$element = 'textarea';
+					$end = '>' . $val['value'] . '</textarea>';
+					break;
+					
+				case 'select':
+					$element = 'select';
+					$end = '>' . $this->_output_options_select($val['options']) . '
+					</select>';
+					break;
+				
+				case 'checkbox':
+					if (count($val['options']) > 0) :
+						$element = '';
+						$end = $this->_output_options_checkbox($val['options'], $val['name']);
+						$label_html = '<p class="checkbox_header">' . $val['label'] . '</p>';
+						break;
+					endif;
+				
+				case 'radio':
+					if (count($val['options']) > 0) :
+						$element = '';
+						$end = $this->_output_options_radios($val['options'], $val['name']);
+						$label_html = '<p class="checkbox_header">' . $val['label'] . '</p>';
+						break;
+					endif;
+				
+				case 'range':
+				case 'number':
+					$min_max_range .= !empty($val['min']) ? ' min="' . $val['min'] . '"' : '';
+					$min_max_range .= !empty($val['max']) ? ' max="' . $val['max'] . '"' : '';
+					$min_max_range .= !empty($val['step']) ? ' step="' . $val['step'] . '"' : '';
+				
+				case 'submit':
+					$this->has_submit = true;
+					break;
+				
+				default :
+					$element = 'input';
+					$end .= ' type="' . $val['type'] . '" value="' . $val['value'] . '"';
+					$end .= $val['checked'] ? ' selected' : '';
+					$end .= $this->field_close();
+					break;
+				
+			endswitch;
+			
+			$id = !empty($val['id']) ? ' id="' . $val['id'] . '"' : '';
+			$class = count($val['class']) ? ' class="' . $this->_output_classes($val['class']) . '"' : '';
+			$attr = $val['autofocus'] ? ' autofocus' : '';
+			$attr = $val['checked'] ? ' checked' : '';
+			$attr = $val['required'] ? ' required' : '';
+			
+			// Build the label
+			if (!empty($label_html) && $val['type'] != 'hidden' && $val['type'] != 'submit') :
+				$field .= $label_html;
+			elseif ($val['add_label']) :
+				$val['label'] .= $val['required'] ? ' <strong>*</strong>' : '';
+				$field .= '
+					<label for="' . $val['id'] . '">' . $val['label'] . '</label>';
+			endif;
+			
+			if (!empty($element))
+				$field .= '
+					<' . $element . $id . ' name="' . $val['name'] . '"' . $min_max_range . $attr . $end;
+			else 
+				$field .= $end;
+			
+			// Parse and create wrap, if needed
+			if ($val['type'] != 'hidden' && !empty($val['wrap_tag'])) :
+			
+				$wrap_before = '
+				<' . $val['wrap_tag'];
+				$wrap_before .= count($val['wrap_class']) > 0 ? $this->_output_classes($val['wrap_class']) : '';
+				$wrap_before .= !empty($val['wrap_style']) ? ' style="' . $val['wrap_style'] . '"' : '';
+				$wrap_before .= !empty($val['wrap_id']) ? ' id="' . $val['wrap_id'] . '"' : '';
+				$wrap_before .= '>';
+				
+				$wrap_after = '
+				</' . $val['wrap_tag'] . '>';
+				
+				$output .= $wrap_before . $field . $wrap_after;
+			else : 
+				$output .= $field;
+			endif;
+			
+		endforeach;	
+		
+		if (! $this->has_submit) $output .= '
+				<div class="' . $val['wrap_class'] . '">
+					<input type="submit" value="Submit" name="submit">
+				</div>';
+		
+		$output .= '
+		</form>';
+		
+		echo $output;
+		
+	}
+	
+	private function _check_valid_attr($string) {
+		
+		$result = true;
+		
+		// Check $name for correct characters
+		// "^[a-zA-Z0-9_-]*$"
+		
+		return $result;
+		
+	}
+	
+	function field_close() {
+			
+			return $this->form['markup'] === 'xhtml' ? ' />' : '>';
+			
+	}
+	
+	
+	// Create a slug from a label name
+	private function _make_slug($string) {
+		
+		$result = '';
+		
+		$result = str_replace('"', '', $string);
+		$result = str_replace("'", '', $result);
+		$result = str_replace('_', '-', $result);
+		$result = preg_replace('~[\W\s]~', '-', $result);
+		
+		$result = strtolower($result);
+		
+		return $result;
+		
+	}
+	
+	private function _output_classes($arr) {
+		
+		$output = '';
+		
+		if (count($arr) > 0) :
+			$output .= ' class="';
+			foreach ($arr as $class) :
+				$output .= $class . ' ';
+			endforeach;
+			$output .= '"';
+		endif;
+		
+		return $output;
+		
+	}
+	
+	private function _output_options_select($arr) {
+		$output = '';
+		foreach ($arr as $val => $opt) :
+			$output .= '
+						<option value="' . $val . '">' . $opt . '</option>';
+		endforeach;
+		return $output;
+	}
+	
+	private function _output_options_radios($arr, $name) {
+		$output = '';
+		foreach ($arr as $val => $opt) :
+			$slug = $this->_make_slug($opt);
+			$output .= '
+						<input type="radio" name="' . $name . '[]" value="' . $val . '" id="' . $slug . '"';
+			$output .= $this->form['markup'] === 'xhtml' ? ' />' : '>';
+			$output .= '
+						<label for="' . $slug . '">' . $opt . '</label>';
+		endforeach;
+		return $output;
+	}
+	
+	private function _output_options_checkbox($arr, $name) {
+		$output = '';
+		foreach ($arr as $val => $opt) :
+			$slug = $this->_make_slug($opt);
+			$output .= '
+						<input type="checkbox" name="' . $name . '[]" value="' . $val . '" id="' . $slug . '"';
+			$output .= $this->form['markup'] === 'xhtml' ? ' />' : '>';
+			$output .= '
+						<label for="' . $slug . '">' . $opt . '</label>';
+		endforeach;
+		return $output;
+	}
+	
+}
---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+
+<?php
+
+echo '<pre>';print_r($_REQUEST);echo '</pre>';
+echo '<pre>';print_r($_FILES);echo '</pre>';
+
+require_once('FormBuilder.php');
+	
+/*
+Create a new instance
+Pass in a URL to set the action
+*/
+$form = new ThatFormBuilder();
+
+/*
+Form attributes are modified with the set_att function.
+First argument is the setting
+Second argument is the value
+*/
+/*$form->set_att('method', 'post');
+$form->set_att('enctype', 'a_contact_form');
+$form->set_att('markup', 'html');
+$form->set_att('class', 'class_1');
+$form->set_att('class', 'class_2');
+$form->set_att('id', 'a_contact_form');
+$form->set_att('novalidate', true);
+$form->set_att('add_honeypot', false);
+$form->set_att('add_nonce', 'a_contact_form');
+
+*/
+
+$form->set_att('enctype', 'multipart/form-data');
+$form->set_att('method', 'post');
+
+/*
+Uss add_input to create form fields
+First argument is the name
+Second argument is an array of arguments for the field
+Third argument is an alternative name field, if needed
+*/
+$form->add_input('Name', array(
+), 'contact_name');
+
+$form->add_input('Email', array(
+	'type' => 'email'
+), 'contact_email');
+
+$form->add_input('Filez', array(
+	'type' => 'file'
+), 'filez_bitch');
+
+$form->add_input('Reason for contacting', array(
+	'type' => 'checkbox',
+	'options' => array(
+		'say_hi' => 'Just saying hi!',
+		'complain' => 'I have a bone to pick',
+		'offer_gift' => 'I\'d like to give you something neat',
+	)
+));
+
+$form->add_input('Bad Headline', array(
+	'type' => 'radio',
+	'options' => array(
+		'say_hi_2' => 'Just saying hi! 2',
+		'complain_2' => 'I have a bone to pick 2',
+		'offer_gift_2' => 'I\'d like to give you something neat 2',
+	)
+));
+
+$form->add_input('Reason for contact', array(
+	'type' => 'select',
+	'options' => array(
+		'' => 'Select...',
+		'say_hi' => 'Just saying hi!',
+		'complain' => 'I have a bone to pick',
+		'offer_gift' => 'I\'d like to give you something neat',
+	)
+));
+
+$form->add_input('Question or comment', array(
+	'required' => true,
+	'type' => 'textarea',
+	'value' => 'Type away!'
+));
+
+$form->add_inputs(array(
+	array('Field 1'),
+	array('Field 2'),
+	array('Field 3')
+));
+
+/*
+Create the form
+*/
+$form->build_form();
+?>
+</body>
+</html>
